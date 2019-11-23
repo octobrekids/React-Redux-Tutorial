539297fafb70edeea394c287e1cc3794b313c7c6
octobrekids <chonrada.k@mail.kmutt.ac.th>
2019-11-24 02:06:28 +0700

    nesting component

---
+++ my-app/src/jungkook.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class jungkook extends Component {
+    render() {
+        return (
+            <div className="jungkook">
+                <div>Name: 'Jungkook'</div>
+                <div>Age: 25</div>
+                
+            </div>
+        )
+    }
+}
