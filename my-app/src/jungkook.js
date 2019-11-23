56411b96ddaf4c97708883b520c7b884f90e4ec0
octobrekids <chonrada.k@mail.kmutt.ac.th>
2019-11-24 02:47:15 +0700

    conditional output

--- my-app/src/jungkook.js
+++ my-app/src/jungkook.js
@@ -1,21 +1,29 @@
 import React from "react";
 
-const Jungkook = (props) => {
-    const { ninjas } = props;
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className="jungkook" key={ ninja.id }>
-          <div>Name: { ninja.name }</div>
-          <div>Age: { ninja.age }</div>
-        </div>
-      );
-    });
-    return(
-        <div className="ninja-list">
-            { ninjaList }
-        </div>
-    )
-}
+const Jungkook = props => {
+  const { ninjas } = props;
+  //   const ninjaList = ninjas.map(ninja => {
+  //     if (ninja.age > 20) {
+  //       return (
+  //         <div className="jungkook" key={ninja.id}>
+  //           <div>Name: {ninja.name}</div>
+  //           <div>Age: {ninja.age}</div>
+  //         </div>
+  //       );
+  //     } else {
+  //       return null;
+  //     }
+  //   });
+  const ninjaList = ninjas.map(ninja => {
+   return ninja.age > 20 ? (
+      <div className="jungkook" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+      </div>
+    ) : null;
+  });
+  
+  return <div className="ninja-list">{ninjaList}</div>;
+};
 
 export default Jungkook;
- 
